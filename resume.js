5004ca1431a3d49b6e834bd177eb6821f6dceb9b
Kaveen Raj <Kaveenrajm99@gmail.com>
2022-01-19 21:57:06 +0530

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,14 @@
+let resume={
+    "name":"KaveenRaj",
+    "mailid":"kaveenrajm99@gmail.com",
+    "qualification":"BE Mechanical",
+    "collegename":"sec",
+    "collegeplace":"tirchengode",
+    "areaofinterest":"software developer",
+    "hobbies":"riding",
+    "computerproficiency":["javascript","networking","system engineer"],
+    "percentage":"74",
+    "languageknown":["english","tamil"],
+    "native":"erode"
+}
+console.log(resume);
